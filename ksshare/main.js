98a996a5bf4bcf4ec58910b2b40b6f3246d412d6
structur3dprinting <andrew@structur3d.io>
2014-06-09 13:45:39 -0400

    Update main.js
    url change

--- ksshare/main.js
+++ ksshare/main.js
@@ -15,15 +15,15 @@ $(function() {
             
             
             if ((screen.width < 480) || (screen.height < 480)) {
-                window.open ('http://www.facebook.com/share.php?u=http://kck.st/1mVwhmT', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+                window.open ('http://www.facebook.com/share.php?u=http://kck.st/1kKQwEl', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
             } else {
-                window.open ('http://www.facebook.com/share.php?u=http://kck.st/1mVwhmT', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);   
+                window.open ('http://www.facebook.com/share.php?u=http://kck.st/1kKQwEl', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);   
             }
     });
     
     $('.twitter').on('click', function() {
-        var loc = encodeURIComponent('http://kck.st/1mVwhmT'),
-                title = "Discov3ry Extruder: Do more than ever with YOUR 3D Printer by @Structur3DPrint — ",
+        var loc = encodeURIComponent('http://kck.st/1kKQwEl'),
+                title = "Check out @structur3Dprint's pastruder for #3Dprinting. Go beyond plastic: print silicone, nutella, woodfiller + more ",
                 w = 580, h = 300,
                 left = (screen.width/2)-(w/2),
                 top = (screen.height/2)-(h/2);
@@ -32,7 +32,7 @@ $(function() {
     });
     
     $('.play').on('click', function() {
-        window.location.href = "http://kck.st/1mVwhmT";
+        window.location.href = "http://kck.st/1kKQwEl";
     });
     
 });
